4b01628713a03a3e75dae1fe9e8994c71c7da8b8
clycrs <carolinelaycuras@hotmail.fr>
2020-11-19 12:53:20 +0100

    ok model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# rails generate model Cocktails name:string
+# rails generate model Ingredients name:string
+# rails generate model Doses description:string cocktails:references ingredients:references
+# rails db:migrate
